192adf8e8300a81562327e52aa4d2901e07cc5c1
Toh Heng Xing <102740235+tohhengxing@users.noreply.github.com>
2025-06-09 21:07:05 +0800

    feat: Add annotations to view document page
    Also, get rid of single click causing zooming in on an image.

--- app/javascript/controllers/viewer_controller.js
+++ app/javascript/controllers/viewer_controller.js
@@ -1,12 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 import OpenSeadragon from "openseadragon"
+import {createOSDAnnotator, UserSelectAction} from "@annotorious/openseadragon";
 
 export default class extends Controller {
     static targets = ["container"]
-    static values = { imageUrl: String }
+    static values = { imageUrl: String, annotations: String }
+
+    allAnnotations = []
 
     connect() {
-        console.log(this.imageUrlValue)
+        console.log(this.annotationsValue)
         this.viewer = OpenSeadragon({
             id: this.containerTarget.id,
             prefixUrl: "https://cdn.jsdelivr.net/npm/openseadragon@4/build/openseadragon/images/",
@@ -14,8 +17,24 @@ export default class extends Controller {
                 type: 'image',
                 url: this.imageUrlValue
             },
-            showFullPageControl: false
+            showFullPageControl: false,
+            gestureSettingsMouse: {
+                clickToZoom: false
+            }
         })
+
+        this.anno = createOSDAnnotator(this.viewer, {
+            drawingEnabled: false,
+            userSelectAction: UserSelectAction.SELECT,
+            style: {
+                fill: '#ff0000',
+                fillOpacity: 0.25
+            }
+        });
+
+        this.allAnnotations = JSON.parse(this.annotationsValue)
+        this.anno.setAnnotations(this.allAnnotations)
+
     }
 
     disconnect() {
