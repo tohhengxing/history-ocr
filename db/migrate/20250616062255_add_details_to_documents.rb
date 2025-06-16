d5dafefaaa7b798c8189dbeb5337dbb6b94d84f7
Toh Heng Xing <102740235+tohhengxing@users.noreply.github.com>
2025-06-16 14:27:51 +0800

    feat: Add details to documents

---
+++ db/migrate/20250616062255_add_details_to_documents.rb
@@ -0,0 +1,7 @@
+class AddDetailsToDocuments < ActiveRecord::Migration[8.0]
+  def change
+    add_column :documents, :transcription, :text
+    add_column :documents, :translation, :text
+    add_column :documents, :transliteration, :text
+  end
+end
