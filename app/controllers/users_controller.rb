6c46ed9d8a118dfd473890c8a50889447877d898
Toh Heng Xing <102740235+tohhengxing@users.noreply.github.com>
2025-06-17 16:30:19 +0800

    feat: Add user destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash.now[:notice] = "User created!"
       respond_to do |format|
         format.turbo_stream
         format.html { redirect_to users_path }
@@ -41,6 +42,12 @@ class UsersController < ApplicationController
       flash.now[:error] = "Invalid inputs"
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: "Document was successfully destroyed"
   end
 
   private
